2861da29e9161d8d0ca422e4d570ba82a2697b4c
ivalencius <ilanvalencius@gmail.com>
2021-03-23 22:43:25 -0400

    switched path for DIR_ROOT

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import sys
 
 home_path = os.getcwd()
 DIR_ROOT = os.path.dirname(os.path.abspath(__file__))
-# Change when not testing
-DIR_DATA = os.path.join(DIR_ROOT, 'test_data/')
+# Change when testing
+DIR_DATA = os.path.join(DIR_ROOT, 'data/')
 DIR_SORTED = os.path.join(DIR_ROOT, 'sorted_nGrams/')
 
